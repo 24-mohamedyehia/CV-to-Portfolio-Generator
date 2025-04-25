1debfe1ccfa52a4d97319c57a36fa460d91a1971
mohamed-y <24-mohamedyehia>
2025-04-25 03:49:03 +0300

    refactor: update agent definitions and add task implementations in agent files

--- src/agents/cv_analyzer_agent.py
+++ src/agents/cv_analyzer_agent.py
@@ -1,10 +1,20 @@
 # agents/cv_analyzer_agent.py
-from . import Agent , basic_llm
+from crewai import Agent, Task
+import os
+from src.agents.llms import basic_llm
+output_dir = "src/ai-agent-output"
 
-cv_analyzer_agent = Agent(
+cv_analyzer = Agent(
     role='CV Analyzer Agent',
     goal='Extract skills, experience, education and projects from the CV text.',
     backstory='You are a professional recruiter who knows how to read and understand CVs.',
     llm= basic_llm,
     verbose=True
+)
+
+analyze_task = Task(
+    description="Analyze the CV plan text",
+    expected_output="JSON with skills, experience, education, and projects.",
+    output_file=os.path.join(output_dir, "step_2_analysis.json"),
+    agent=cv_analyzer
 )
--- src/agents/cv_reader_agent.py
+++ src/agents/cv_reader_agent.py
@@ -1,7 +1,10 @@
 # agents/cv_reader_agent.py
-from . import Agent , basic_llm 
+from crewai import Agent, Task 
+import os
+from src.agents.llms import basic_llm
+output_dir = "src/ai-agent-output"
 
-cv_reader_agent = Agent(
+cv_reader = Agent(
     role=  "CV Reader Agent",
     goal=  "Extract text content from the uploaded CV",
     backstory= "Expert at reading and extracting content from CVs",
@@ -9,3 +12,11 @@ cv_reader_agent = Agent(
     verbose= True
 )
 
+
+
+reader_task = Task(
+    description="Read this {cv_text} CV and extract the text content.",
+    expected_output="Plain text of the CV.",
+    output_file=os.path.join(output_dir, "step_1_cv_text.txt"),
+    agent=cv_reader
+)

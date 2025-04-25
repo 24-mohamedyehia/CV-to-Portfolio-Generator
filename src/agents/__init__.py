f298af5672bb283f4ce61a619e5cefe42fd93d0d
mohamed-y <24-mohamedyehia>
2025-04-25 20:47:10 +0300

    feat: add main application logic and agent imports to src

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,5 +1,4 @@
 # agent/__init__.py
-from src.agents.cv_reader_agent import cv_reader, reader_task
-from src.agents.cv_analyzer_agent import cv_analyzer , analyze_task
-from src.agents.web_generator_agent import web_generator , website_task
-
+from .cv_reader_agent import cv_reader, reader_task
+from .cv_analyzer_agent import cv_analyzer, analyze_task
+from .web_generator_agent import web_generator, website_task

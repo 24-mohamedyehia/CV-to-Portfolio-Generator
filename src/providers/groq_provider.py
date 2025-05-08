cfdbac57b996d881c09e4dd462c7960abbc8f944
mohamed-y <24-mohamedyehia>
2025-05-08 14:55:19 +0300

    refactor: update groq_provider.py to correct model list and redefine basic_llm

--- src/providers/groq_provider.py
+++ src/providers/groq_provider.py
@@ -1,4 +1,3 @@
-
 from crewai import LLM
 import os
 from dotenv import load_dotenv
@@ -9,12 +8,19 @@ groq_api_key = os.getenv("GROQ_API_KEY")
 Here is the list of available models:
 
 llama-3.3-70b-versatile
-qwen-qwq-32b
+gemma2-9b-it
+llama3-70b-8192
 
 """
 
-basic_llm = LLM(
+llama_3_3_70B_llm = LLM(
     model="groq/llama-3.3-70b-versatile",
+    temperature=0,
+    api_key=groq_api_key,
+)
+
+llama_4_llm = LLM(
+    model="groq/meta-llama/llama-4-scout-17b-16e-instruct",
     temperature=0,
     api_key=groq_api_key
 )

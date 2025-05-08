addc4320688b96942db35022c9431545917cf74f
mohamed-y <24-mohamedyehia>
2025-05-08 14:54:26 +0300

    refactor: update imports in __init__.py to include html_generator_agent and llama providers

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,3 +1,4 @@
 # agent/__init__.py
 from .reader_analyzer_agent import reader_analyzer, json_extraction_task
-from src.providers import basic_llm 
+from .html_generator_agent import html_generator, html_generator_task
+from src.providers import llama_3_3_70B_llm , llama_4_llm

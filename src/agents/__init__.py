3670a04c8d36bfc01e2fd456dc9d60c7a455fac0
mohamed-y <24-mohamedyehia>
2025-06-08 20:39:53 +0300

    refactor: remove reader_analyzer_agent and update html_generator_agent to use read_pdf tool

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,4 +1,1 @@
-# agent/__init__.py
-from .reader_analyzer_agent import reader_analyzer, json_extraction_task
 from .html_generator_agent import html_generator, html_generator_task
-from src.providers import llama_3_3_70B_llm , llama_4_llm
--- src/agents/html_generator_agent.py
+++ src/agents/html_generator_agent.py
@@ -1,16 +1,17 @@
-import json
 import os
+import pdfplumber
 from crewai import Agent, Task
 from crewai.tools import tool
-from src.providers import llama_4_llm
+from src.providers import basic_llm
 
 output_dir = "src/ai-agent-output"
 
+# Define tools for reading PDF files
 @tool
-def read_json(file_path):
-    """This function reads a JSON file and returns its content as a dictionary or object."""
-    with open(file_path, 'r', encoding='utf-8') as f:
-        return json.load(f)
+def read_pdf(file_path: str) -> str:
+    '''this tool used for reading a PDF file and returns its content as a string or text'''
+    with pdfplumber.open(file_path) as pdf:
+        return "\\n".join([page.extract_text() for page in pdf.pages])
 
 html_generator = Agent(
     role="Professional Portfolio websites Generator",
@@ -21,8 +22,8 @@ html_generator = Agent(
         "You use Bootstrap, TailwindCSS, and write clean semantic HTML. "
         "You care about colors, fonts, margins, responsiveness, and accessibility."
     ),
-    tools=[read_json],
-    llm=llama_4_llm,
+    tools=[read_pdf],
+    llm=basic_llm,
     verbose=True,
     max_iter=2,
     max_rpm=29
@@ -30,8 +31,7 @@ html_generator = Agent(
 
 html_generator_task = Task(
     description="\n".join([
-        "First, read the CV data from the JSON file at {cv_json}",
-        "Task:",
+        "Your job is to carefully read and analyze the uploaded CV {cv_pdf} and generate a single HTML file that includes all sections of the CV.",
         "- <head> includes Bootstrap and Tailwind CSS via CDN.",
         "- <style> block contains the theme CSS inline, using the design specifications (colors, fonts, layout).",
         "- <body> sections: About Me, Skills, Experience, Education, Projects, Contact.",
@@ -50,4 +50,3 @@ html_generator_task = Task(
     output_file=os.path.join(output_dir, "final_portfolio.html"),
     agent=html_generator
 )
-
